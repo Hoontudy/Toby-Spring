01e4dbd12e37924a898cc4445944531d42690c40
Gosh95 <goshk1995@gmail.com>
2023-03-05 00:10:21 +0900

    훈터디 토비의 스프링 예제 코드용 프로젝트 생성

---
+++ la/src/test/java/hoontudy/toby/la/LaApplicationTests.java
@@ -0,0 +1,13 @@
+package hoontudy.toby.la;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
