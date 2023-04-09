0186d899214eb25c0de24ee4ed3ecf8a5a1901f9
Gosh95 <pocal777@naver.com>
2023-04-10 00:36:59 +0900

    1.8 XML을 이용한 설정 예제 실습 및 내용 정리

--- la/src/main/java/hoontudy/toby/la/examples/DaoFactory.java
+++ la/src/main/java/hoontudy/toby/la/examples/DaoFactory.java
@@ -1,5 +1,7 @@
 package hoontudy.toby.la.examples;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +10,18 @@ public class DaoFactory {
 
   @Bean
   public UserDao userDao() {
-    return new UserDao(connectionMaker());
+    UserDao userDao = new UserDao();
+    userDao.setDataSource(dataSource());
+    return userDao;
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    MysqlDataSource dataSource = new MysqlDataSource();
+    dataSource.setUrl("jdbc:mysql://localhost:3306/springbook");
+    dataSource.setUser("root");
+    dataSource.setPassword("1234");
+    return dataSource;
   }
 
   @Bean
